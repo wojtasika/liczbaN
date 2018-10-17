4ed3c8126acb3d015775a05cb789a51ec1d9ee78
Agnieszka Wojtasik <wojtasikagnieszka2626@gmail.com>
2018-10-17 23:14:57 +0200

    plik

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let n = 133;
+
+    if (n < 100) {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+    
+ else if (n > 200) {
+    console.log('Liczba nie znajduje się w przedziale');
+}
+else{
+    console.log('Liczba znajduje się w przedziale');
+} 
+
+    
